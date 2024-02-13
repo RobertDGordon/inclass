7b750e47c3b370ef55a63840ea920f98c87721ac
Robert Gordon <gordonrobertd@gmail.com>
2024-02-12 18:30:34 -0800

    Say something cool to the world!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8000
 app.use(express.json())
 
 app.use('/', (req, res)=>{
-  res.send({ status: 200, message: "Hello world!"})
+  res.send({ status: 200, message: "Hello world, what's up!"})
 })
 
 app.listen(port, ()=> {
